320795b5164af4bd86f07f1e388b4027d7969c83
Patrick Höhn <hoehnp@gmx.de>
2021-05-06 10:42:18 +0200

    initial commit

---
+++ install-packages.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+apt-get update
+apt-get install build-essential flex bison cmake zlib1g-dev \
+                libboost-system-dev libboost-thread-dev curl time \
+                libopenmpi-dev openmpi-bin gnuplot libreadline-dev \
+                libncurses-dev libxt-dev libscotch-dev libptscotch-dev \
+                libvtk6-dev python-numpy git-core qt4-dev-tools vim \
+                libqt4-dev libqt4-opengl-dev freeglut3-dev libqtwebkit-dev  -y
+export QT_SELECT=qt4
+
